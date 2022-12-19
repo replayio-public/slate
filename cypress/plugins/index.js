ba8455cf04bd6f2ae42f343060178c391a5f2338
Jaril <jarilvalenciano@gmail.com>
2022-12-19 15:23:53 -0500

    Track metrics as action-finished-2

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,5 @@
 const cypressReplay = require('@replayio/cypress')
+const { writeFileSync } = require('fs')
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -19,5 +20,12 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   cypressReplay.default(on, config)
+
+  on('after:run', afterRun => {
+    const data = JSON.stringify(afterRun.totalDuration)
+    const filename = 'duration.json'
+    writeFileSync(filename, data)
+    console.log('cypress-json-results: wrote results to %s', filename)
+  })
   return config
 }
